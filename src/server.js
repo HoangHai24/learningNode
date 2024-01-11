f0a8248617a7fc1fe3e6bcbac73864c078369c27
HoangHai24 <maihoanghai6789@gmail.com>
2024-01-12 00:06:55 +0700

    update

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const mysql = require("mysql2");
 
 const app = express(); // app express
 const port = process.env.PORT || 3001;
@@ -11,6 +12,20 @@ const hostname = process.env.HOST_NAME || require("dotenv").config();
 configViewEngine(app);
 
 app.use("/", webRoutes);
+
+//test connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "demodocker",
+});
+connection.query("select * from Users", (err, result, fields) => {
+  console.log("result=>>", result);
+  console.log("fields=>>", fields);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
